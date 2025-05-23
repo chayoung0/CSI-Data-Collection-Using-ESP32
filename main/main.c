3c9f17da372bd0ad43cc0a4474ab2e69efa915c9
chayoung <viveca649@gmail.com>
2025-05-23 23:08:47 +0300

    exploring binary semaphore

--- main/main.c
+++ main/main.c
@@ -2,27 +2,36 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+SemaphoreHandle_t xSemaphore;
+
 void vTask1(void *pvParameters) {
-    for(int i=0; i<5; i++){
-        printf("Task1: %d\n", i);
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1000 ms
+    while(1) {
+        if (xSemaphoreTake(xSemaphore, portMAX_DELAY)) {
+            printf("Task1 got the semaphore!\n");
+            xSemaphoreGive(xSemaphore);  // Give it back
+            vTaskDelay(pdMS_TO_TICKS(1000));
+        }
     }
-    vTaskDelete(NULL); // Cleanly delete task when done
 }
 
 void vTask2(void *pvParameters) {
-    for(int i=0; i<10; i++){
-        printf("Task2: %d\n", i);
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1000 ms
+    while(1) {
+        if (xSemaphoreTake(xSemaphore, portMAX_DELAY)) {
+            printf("Task2 got the semaphore!\n");
+            xSemaphoreGive(xSemaphore);  // Give it back
+            vTaskDelay(pdMS_TO_TICKS(1000));
+        }
     }
-    vTaskDelete(NULL); // Cleanly delete task when done
 }
 
 void app_main(void)
 {
     // vTaskStartScheduler(); is not needed in ESP-IDF. ESP-IDF starts the scheduler automatically after app_main() returns.
 
-    xTaskCreate(vTask1, "Task 1", 2048, NULL, 1, NULL);
-    xTaskCreate(vTask2, "Task 2", 2048, NULL, 2, NULL);
+    xSemaphore = xSemaphoreCreateBinary();
+    xSemaphoreGive(xSemaphore); // Initial release (so someone can take it)
+
+    xTaskCreate(vTask1, "Task1", 2048, NULL, 1, NULL);
+    xTaskCreate(vTask2, "Task2", 2048, NULL, 1, NULL);
 
 }
